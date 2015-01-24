240e1b2611507ee5b67c3dbb5a993999bc83caac
Bruno Daniel <bruno@vault7.com>
2015-01-24 04:01:29 -0200

    Added generic action libraries

---
+++ Generic Actions/NavMeshActions.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Hivemind {
+
+	public class NavMeshActions : ActionLibrary {
+		
+		[Hivemind.Action]
+		[Hivemind.Expects("gameObject", typeof(GameObject))]
+		public Hivemind.Result MoveToGameObject() {
+			GameObject go = context.Get<GameObject> ("gameObject");
+			
+			NavMeshAgent navMeshAgent = agent.GetComponent<NavMeshAgent>();
+			Animator anim = agent.GetComponent<Animator>();
+			NavMeshHit sampledDestination;
+			NavMesh.SamplePosition(go.transform.position, out sampledDestination, 10f, 1);
+			float distance = Vector3.Distance (agent.transform.position, sampledDestination.position);
+			
+			// Planning path or moving towards destination
+			if (navMeshAgent.pathPending || navMeshAgent.hasPath && distance > navMeshAgent.stoppingDistance) {
+				return new Hivemind.Result { status = Hivemind.Status.Running };
+			}
+			
+			// Reached destination
+			else if (distance <= navMeshAgent.stoppingDistance) {
+				anim.SetBool ("IsWalking", false);
+				return new Hivemind.Result { status = Hivemind.Status.Success };
+			}
+			
+			// Can't reach destination
+			else if (navMeshAgent.pathStatus == NavMeshPathStatus.PathInvalid) {
+				return new Hivemind.Result { status = Hivemind.Status.Failure };
+			}
+			
+			// Set destination
+			else {
+				navMeshAgent.SetDestination(sampledDestination.position);
+				anim.SetBool ("IsWalking", true);
+				return new Hivemind.Result { status = Hivemind.Status.Running };
+			}
+		}
+
+		[Hivemind.Action]
+		public Hivemind.Result MoveTo() {
+			
+			NavMeshAgent navMeshAgent = agent.GetComponent<NavMeshAgent>();
+			Animator anim = agent.GetComponent<Animator>();
+			NavMeshHit sampledDestination;
+			NavMesh.SamplePosition(Vector3.zero, out sampledDestination, 10f, 1);
+			float distance = Vector3.Distance (agent.transform.position, sampledDestination.position);
+			
+			// Planning path or moving towards destination
+			if (navMeshAgent.pathPending || navMeshAgent.hasPath && distance > navMeshAgent.stoppingDistance) {
+				return new Hivemind.Result { status = Hivemind.Status.Running };
+			}
+			
+			// Reached destination
+			else if (distance <= navMeshAgent.stoppingDistance) {
+				anim.SetBool ("IsWalking", false);
+				return new Hivemind.Result { status = Hivemind.Status.Success };
+			}
+			
+			// Can't reach destination
+			else if (navMeshAgent.pathStatus == NavMeshPathStatus.PathInvalid) {
+				return new Hivemind.Result { status = Hivemind.Status.Failure };
+			}
+			
+			// Set destination
+			else {
+				navMeshAgent.SetDestination(sampledDestination.position);
+				anim.SetBool ("IsWalking", true);
+				return new Hivemind.Result { status = Hivemind.Status.Running };
+			}
+		}
+
+	}
+	
+
+}
---
+++ Generic Actions/UtilityActions.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Hivemind {
+	
+	public class UtilityActions : ActionLibrary {
+
+		[Hivemind.Action("tag", "maxDistance")]
+		[Hivemind.Outputs("gameObject", typeof(GameObject))]
+		public Hivemind.Result FindNearestObjectWithTag(string tag, float maxDistance) {
+			
+			GameObject[] nodes = GameObject.FindGameObjectsWithTag (tag);
+			
+			float nearestDistance = float.PositiveInfinity;
+			GameObject nearestNode = null;
+			foreach (GameObject node in nodes) {
+				float distance = Vector3.Distance(node.transform.position, agent.transform.position);
+				if (distance < nearestDistance) {
+					nearestDistance = distance;
+					nearestNode = node;
+				}
+			}
+			
+			if (nearestNode != null) {
+				context.Set<GameObject> ("gameObject", nearestNode);
+				return new Hivemind.Result { status=Hivemind.Status.Success };
+			} else {
+				return new Hivemind.Result { status=Hivemind.Status.Failure };
+			}
+		}
+		
+		[Hivemind.Action("seconds")]
+		public Hivemind.Result Wait(float seconds) {
+			float time = context.Get<float>("timeWaited", 0f);
+			if (time < seconds) {
+				time += Time.deltaTime;
+				context.Set<float>("timeWaited", time);
+				return new Hivemind.Result { status = Hivemind.Status.Running };
+			} else {
+				context.Unset("timeWaited");
+				return new Hivemind.Result { status = Hivemind.Status.Success };
+			}
+		}
+		
+	}
+	
+	
+}
